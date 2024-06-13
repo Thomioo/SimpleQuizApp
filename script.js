3da462bde3f63e684c0c44f6e722b48522e4a259
Thomioo <88208504+Thomioo@users.noreply.github.com>
2024-06-13 21:31:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,430 @@
+data = {
+    "Miříkovité": [
+        "Mrkev",
+        "Petržel",
+        "Miřík",
+        "Pastinák",
+        "Kopr",
+        "Kmín",
+        "Fenikl",
+        "Koriadr",
+        "Libiček",
+        "Andělika",
+        "Anýz",
+        "Bolševník",
+        "Bolehlav",
+        "Tořice",
+        "Hvězdnatec",
+        "Máčka"
+    ],
+    "Vrbovité": [
+        "Vrba",
+        "Topol",
+        "Oncoba",
+        "Idézie",
+        "Caseria",
+        "Dovyalis",
+        "Slivouch"
+    ],
+    "Lilkovité": [
+        "Lilek",
+        "Rajče",
+        "Paprika",
+        "Pepino",
+        "Mochyně",
+        "Kustovnice",
+        "Tabák",
+        "Vitánie",
+        "Durman",
+        "Rulík",
+        "BLín",
+        "Petúnie",
+        "Kladivník",
+        "Lilkovník",
+        "Solander",
+        "Jazylka"
+    ],
+    "Růžovité": [
+        "Jahodník",
+        "Jahodka",
+        "Kontryhel",
+        "Krvavec",
+        "Kuklík",
+        "Mochna",
+        "Ostružník",
+        "Udatna",
+        "Tužebník",
+        "Řepík",
+        "Řepíček",
+        "Růže",
+        "Ostružník",
+        "Trnka",
+        "Tvolník",
+        "Hloh",
+        "Jabloň",
+        "Hrušeň",
+        "Jeřáb",
+        "Slivoň"
+    ],
+    "Mákovité": [
+        "Dymnivka",
+        "Srdcovka",
+        "Zemědým",
+        "Vlaštovičník",
+        "Rokatec",
+        "Mák",
+        "Sluncovka",
+        "Pleskanka",
+        "VLaštovičník"
+    ],
+    "Bukovité": [
+        "Buk",
+        "Dub",
+        "Litokarpus",
+        "Kaštanovec"
+    ],
+    "Hvězdnicovité": [
+        "Sedmikráska",
+        "Kopretina",
+        "Smetánka",
+        "Máchelka",
+        "Jestřábník",
+        "Hadí mord",
+        "Prka",
+        "Podběl",
+        "Řebříček",
+        "Pelyněk",
+        "Vratič",
+        "Zlatobýl",
+        "Pupava",
+        "Hvězdnice",
+        "Čekanka",
+        "Protěž",
+        "Srpice",
+        "Devětsil",
+        "Pcháč",
+        "Chrpa",
+        "Kamzičník",
+        "JIřinka",
+        "Listopadka",
+        "Heřmánek",
+        "Heřmánkovec",
+        "Rmen",
+        "Turan",
+        "Slunečnice",
+        "Locika",
+        "Starček",
+        "Pěťour",
+        "Mléč",
+        "Kapustka",
+        "Bodlák",
+        "Lopuch",
+        "Škarda",
+        "Kozí brada",
+        "Ostálka",
+        "Měsíček",
+        "Aksamitník",
+        "Krásenka"
+    ],
+    "Hluchavkovité": [
+        "Hluchavka",
+        "Bazalka",
+        "Levandule",
+        "Máta",
+        "Mateřídouška",
+        "Meduňka",
+        "Popenec",
+        "Saturejka",
+        "Šanta",
+        "Šalvěj",
+        "Dobromysl(oregano)",
+        "Majoránka",
+        "Černohlávek"
+    ],
+    "Břízovité": [
+        "Bříza",
+        "Olše",
+        "Líska",
+        "Habr",
+        "Habrovec",
+        "Habrovka"
+    ],
+    "Bobovité": [
+        "Hrách",
+        "Čočka",
+        "Fazol",
+        "Cizrna",
+        "Sója",
+        "Bob",
+        "Tolice",
+        "Podzemnice",
+        "Čičorka",
+        "Stínovník",
+        "Jetel",
+        "Vikev",
+        "Hrachor",
+        "Kozinec",
+        "Jestřabina",
+        "Jehlice",
+        "Vistárie",
+        "Středenec",
+        "Trnovník"
+    ],
+    "Pryskyřníkovité": [
+        "Pryskyřník",
+        "Blatouch",
+        "Sasanka",
+        "Orsej",
+        "Oměj",
+        "Jaterník",
+        "Koniklec",
+        "Čemeřice",
+        "Hlaváček",
+        "Talovín",
+        "Černucha",
+        "Ostrožka",
+        "Plamének",
+        "Žluťocha",
+        "Lakušník",
+        "Zapalice",
+        "Myší ocásek"
+    ],
+    "Liliovité": [
+        "Lilie",
+        "Tulipán",
+        "Hyacint",
+        "Řebčík",
+        "Kandík",
+        "Liliovka",
+        "Cibule",
+        "Česnek",
+        "Pór",
+        "Pažitka",
+        "Šafrán",
+        "Konvalinka",
+        "Ocún",
+        "Vraní oko"
+    ],
+    "Brukvovité": [
+        "Brukev",
+        "Řepka",
+        "Řeřicha",
+        "Kedluben",
+        "Kapusta",
+        "Brokolice",
+        "Květák",
+        "Ředkev",
+        "Ředkvička",
+        "Hořčice",
+        "Křen",
+        "Zelí",
+        "Roketa",
+        "Řeřišnice",
+        "Tařička",
+        "Barborka",
+        "Penízek",
+        "Kokoška",
+        "Hulevník",
+        "Česnáček"
+    ],
+    "Arekovité": [
+        "Datlovník",
+        "Kokosovník",
+        "Palma",
+        "Rotan",
+        "Areka",
+        "Duma",
+        "Žumara",
+        "Lontar",
+        "Baktris",
+        "Lágovník",
+        "Jubea",
+        "Roystonea",
+        "Brakea"
+    ],
+    "Vstavačovité": [
+        "Bradáček",
+        "Hlístník",
+        "Hlízovec",
+        "Smrkovník",
+        "Běloprstka",
+        "Hlavinka",
+        "Jazýček",
+        "Kruštík",
+        "Okrotice",
+        "Pětiprstka",
+        "Prstnatec",
+        "Rudohlávek",
+        "Střevíčník",
+        "Tořič",
+        "Vemeníček",
+        "Vstavač",
+        "Vanilovník",
+        "Orchidej (Můrovec)"
+    ],
+    "Lipnicovité": [
+        "Lipnice",
+        "Pšenice",
+        "Žito",
+        "Žitovec",
+        "Oves",
+        "Ječmen",
+        "Kukuřice",
+        "Proso",
+        "Čirok",
+        "Rýře",
+        "Třtinovec",
+        "Bambus",
+        "Bojínek",
+        "Psárka",
+        "Jílek",
+        "Srka",
+        "Ovsík",
+        "Kostřava",
+        "Psineček",
+        "Pýr",
+        "Sveřep",
+        "Metlice",
+        "Bér",
+        "Rákos",
+        "Ježatka",
+        "Zaječí ocásek"
+    ],
+    "Chřestovité": [
+        "Chřest",
+        "Sněženka",
+        "Bledule",
+        "Narcis"
+    ],
+    "Zázvorníkovité": [
+        "Zázvor",
+        "Kurkumovník",
+        "Kardamovník"
+    ],
+    "Banánovníkovité": [
+        "Banánovník"
+    ],
+    "Strelicovité": [
+        "Strelíce"
+    ],
+    "Broméliovité": [
+        "Ananasovník",
+        "Bromélie"
+    ],
+    "Vavřínovité": [
+        "Skořicovník",
+        "Vavřín"
+    ],
+    "Meruzalkovité": [
+        "Rybíz",
+        "Srstka"
+    ],
+    "Konopovité": [
+        "Chmel",
+        "Konopí"
+    ],
+    "Tykvovité ": [
+        "Lubenice",
+        "Okurka",
+        "Tykev"
+    ],
+    "Slézovité": [
+        "Lípa",
+        "Ibišek"
+    ]
+}
+
+function generate() {
+    const final = [];
+    const dataKeys = Object.keys(data);
+
+    dataKeys.forEach(correct => {
+        data[correct].forEach(question => {
+            const possible = dataKeys.slice(); // Clone dataKeys
+            const correctIndex = possible.indexOf(correct);
+            if (correctIndex > -1) {
+                possible.splice(correctIndex, 1); // Remove correct from possible
+            }
+            const options = [correct];
+
+            for (let x = 0; x < 3; x++) {
+                const randomIndex = Math.floor(Math.random() * possible.length);
+                options.push(possible.splice(randomIndex, 1)[0]);
+            }
+
+            options.sort(() => Math.random() - 0.5); // Shuffle options
+            final.push({ options, question, correct });
+        });
+    });
+
+    final.sort(() => Math.random() - 0.5); // Shuffle final array
+    return final;
+}
+
+let all;
+let index = 0
+
+function loadSession() {
+    all = generate();
+    loadQuestion()
+}
+
+function loadQuestion() {
+    document.getElementById("question").innerHTML = all[index]["question"]
+    document.getElementById("1").innerHTML = all[index]["options"][0]
+    document.getElementById("2").innerHTML = all[index]["options"][1]
+    document.getElementById("3").innerHTML = all[index]["options"][2]
+    document.getElementById("4").innerHTML = all[index]["options"][3]
+}
+
+function test1() {
+    let correct = all[index]["correct"]
+    console.log(correct, all[index]["options"][0]);
+    if (correct == all[index]["options"][0]) {
+        document.getElementById("output").innerHTML = "Správně"
+    }
+    else {
+        document.getElementById("output").innerHTML = "Špatně,\nSprávná odpověď: " + correct
+    }
+    index++;
+    loadQuestion()
+}
+
+function test2() {
+    let correct = all[index]["correct"]
+    console.log(correct, all[index]["options"][1]);
+    if (correct == all[index]["options"][1]) {
+        document.getElementById("output").innerHTML = "Správně"
+    }
+    else {
+        document.getElementById("output").innerHTML = "Špatně,\nSprávná odpověď: " + correct
+    }
+    index++;
+    loadQuestion()
+}
+
+function test3() {
+    let correct = all[index]["correct"]
+    console.log(correct, all[index]["options"][2]);
+    if (correct == all[index]["options"][2]) {
+        document.getElementById("output").innerHTML = "Správně"
+    }
+    else {
+        document.getElementById("output").innerHTML = "Špatně,\nSprávná odpověď: " + correct
+    }
+    index++;
+    loadQuestion()
+}
+
+function test4() {
+    let correct = all[index]["correct"]
+    console.log(correct, all[index]["options"][3]);
+    if (correct == all[index]["options"][3]) {
+        document.getElementById("output").innerHTML = "Správně"
+    }
+    else {
+        document.getElementById("output").innerHTML = "Špatně,\nSprávná odpověď: " + correct
+    }
+    index++;
+    loadQuestion()
+}
